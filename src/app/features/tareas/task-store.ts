a4e71b93abc3adb1bd4083c633559d357fa8321f
Adrian Lara <ajlaras@minsait.com>
2026-07-22 16:40:57 -0600

    fix (ID-) - se agrega el Storage Local

--- src/app/features/tareas/task-store.ts
+++ src/app/features/tareas/task-store.ts
@@ -1,17 +1,20 @@
-import { Component, computed, signal,Injectable } from '@angular/core';
+import { Component, computed, effect,signal,Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Task } from './task';
+
+const STORAGE_KEY = 'tareas';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TaskStore {
+    tareas = signal<Task[]>(this.cargar());
 
-    tareas = signal<Task[]>([
-        { id: 1, titulo: 'Aprender angular', completada: false },
-        { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
-        { id: 3, titulo: 'Dominar signals', completada: true },
-    ]);
+constructor() {
+    effect(() => {
+      localStorage.setItem(STORAGE_KEY, JSON.stringify(this.tareas()));
+    });
+  }
 
     pendientes = computed(() => this.tareas().filter((t) => !t.completada).length);
     totalTareas = computed(() => this.tareas().length);
@@ -43,4 +46,17 @@ export class TaskStore {
             (lista) => lista.filter((t) => t.completada === false)
         );
     }
+
+    private cargar(): Task[] {
+    const guardadas = localStorage.getItem(STORAGE_KEY);
+
+    if (guardadas) {
+      return JSON.parse(guardadas);
+    }
+    return [
+      { id: 1, titulo: 'Aprender angular', completada: false },
+      { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
+      { id: 3, titulo: 'Dominar signals', completada: true },
+    ];
+  }
 }
