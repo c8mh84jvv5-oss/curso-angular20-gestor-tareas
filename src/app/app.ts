8e54b6cc92a26d3b4d41b245707ca38818aad75d
Adrian Lara <ajlaras@minsait.com>
2026-07-22 09:07:05 -0600

    feat (ID-02) - re factorización de código

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
-import { Component, computed, signal } from '@angular/core';
+import { Component, computed, signal,inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Task } from './task';
-
+import { Task } from './features/tareas/task';
+import { TaskStore } from './features/tareas/task-store';
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
@@ -10,34 +10,6 @@ import { Task } from './task';
 })
 export class App {
   protected readonly title = 'Gestor de tareas';
-
-  tareas = signal<Task[]>([
-    { id: 1, titulo: 'Aprender angular', completada: false },
-    { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
-    { id: 3, titulo: 'Dominar signals', completada: true },
-  ]);
-
-  pendientes = computed(() => this.tareas().filter((t) => !t.completada).length);
-
-  agregar(titulo: string): void {
-    const limpio = titulo.trim();
-    if (!limpio) {
-      return;
-    }
-
-    this.tareas.update((lista) => [
-      ...lista,
-      { id: Date.now(), titulo: limpio, completada: false },
-    ]);
-  }
-
-  eliminar(id: number): void {
-    this.tareas.update((lista) => lista.filter((t) => t.id !== id));
-  }
-
-  toggle(id: number): void {
-    this.tareas.update((lista) =>
-      lista.map((t) => (t.id === id ? { ...t, completada: !t.completada } : t)),
-    );
-  }
+taskStore = inject(TaskStore);
+  
 }
---
+++ src/app/features/tareas/task-store.ts
@@ -0,0 +1,41 @@
+import { Component, computed, signal,Injectable } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Task } from './task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskStore {
+
+    tareas = signal<Task[]>([
+    { id: 1, titulo: 'Aprender angular', completada: false },
+    { id: 2, titulo: 'Construir un proyecto nuevo', completada: false },
+    { id: 3, titulo: 'Dominar signals', completada: true },
+  ]);
+
+  pendientes = computed(() => this.tareas().filter((t) => !t.completada).length);
+  totalTareas = computed(() => this.tareas().length);
+
+  agregar(titulo: string): void {
+    const limpio = titulo.trim();
+    if (!limpio) {
+      return;
+    }
+
+    this.tareas.update((lista) => [
+      ...lista,
+      { id: Date.now(), titulo: limpio, completada: false },
+    ]);
+  }
+
+  eliminar(id: number): void {
+    this.tareas.update((lista) => lista.filter((t) => t.id !== id));
+  }
+
+  toggle(id: number): void {
+    this.tareas.update((lista) =>
+      lista.map((t) => (t.id === id ? { ...t, completada: !t.completada } : t)),
+    );
+  }
+    
+}
